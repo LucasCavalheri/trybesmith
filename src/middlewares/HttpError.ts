0ad8179af851e7cd053b525195727adc120267e9
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-14 20:52:09 -0300

    refactor: removes zod

--- src/middlewares/HttpError.ts
+++ src/middlewares/HttpError.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import HttpException from '../errors/HttpException';
-import ZodException from '../errors/ZodException';
 
 function HttpErrorMiddleware(
-  err: HttpException | ZodException,
+  err: HttpException,
   _req: Request,
   res: Response,
   _next: NextFunction,
@@ -12,9 +11,6 @@ function HttpErrorMiddleware(
     return res.status(err.status).json({ message: err.message });
   }
 
-  if (err instanceof ZodException) {
-    return res.status(err.status).json({ message: err.flatten().fieldErrors });
-  }
   const { status, message } = err;
   res.status(status || 500).json({ message });
 }
