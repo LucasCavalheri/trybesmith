42af4c00676375913eb556be8dee3f78ece95d5a
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-14 14:48:06 -0300

    feat: create connection.ts and initiate project

---
+++ src/models/connection.ts
@@ -0,0 +1,10 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+});
