94324d38b9b865a7c58f5745e4dd490eb9249435
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-14 15:47:53 -0300

    feat: add createProduct function on ProductService

---
+++ src/services/ProductService.ts
@@ -0,0 +1,12 @@
+import connection from '../models/connection';
+import { Product } from '../interfaces';
+import ProductModel from '../models/ProductModel';
+
+export default class ProductService {
+  constructor(private productModel = new ProductModel(connection)) {}
+
+  public createProduct = async (product: Product): Promise<Product> => {
+    const newProduct = await this.productModel.createProduct(product);
+    return newProduct;
+  };
+}
