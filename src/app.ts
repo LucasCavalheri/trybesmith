2b08c43e05f041bbb1c641e0d89bb2f49b005e87
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-14 17:46:59 -0300

    feat: add app.use(error)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
+import 'express-async-errors';
 import express from 'express';
+import HttpErrorMiddleware from './middlewares/HttpError';
 import router from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(HttpErrorMiddleware);
 
 export default app;
