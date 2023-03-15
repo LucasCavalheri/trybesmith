29bf1b39ca83bcf5ba2437b7e774539e88b4a34c
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-15 14:31:55 -0300

    feat: add middleware to POST /product router

--- src/routes/ProductRouter.ts
+++ src/routes/ProductRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
+import { validateProduct } from '../middlewares/FieldsMiddleware';
 
 const router = Router();
 
 const productController = new ProductController();
 
 router.get('/', productController.getAllProducts);
-router.post('/', productController.createProduct);
+router.post('/', validateProduct, productController.createProduct);
 
 export default router;
