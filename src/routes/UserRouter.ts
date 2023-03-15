bc9bdb296b50337b76b4fa245dd1a2f7347b58b0
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-15 14:44:40 -0300

    feat: add middleware to POST /user router

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import { validateUser } from '../middlewares/FieldsMiddleware';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/', userController.createUser);
+router.post('/', validateUser, userController.createUser);
 
 export default router;
