400fd642a095f96853ce7d2657f52ada8d7bc36c
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-15 14:44:10 -0300

    feat: add validateUser middleware

--- src/middlewares/FieldsMiddleware.ts
+++ src/middlewares/FieldsMiddleware.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import HttpException from '../errors/HttpException';
-import { Product } from '../interfaces';
+import { Product, User } from '../interfaces';
 import productSchema from '../validations/ProductSchema';
+import userSchema from '../validations/UserSchema';
 
 export const validateProduct = async (
   req: Request,
@@ -21,4 +22,20 @@ export const validateProduct = async (
   next();
 };
 
-export const fn = () => {};
+export const validateUser = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+) => {
+  const user = req.body as User;
+
+  const { error } = userSchema.validate(user);
+
+  if (error?.details[0].type.includes('required')) {
+    throw new HttpException(400, error.message);
+  }
+
+  if (error) throw new HttpException(422, error.message);
+
+  next();
+};
